84bfb269d8857eaa0d3645fb8275d1cf1ffbc4da
CodeYouness <ylijassi@gmail.com>
2024-07-04 00:50:33 +0200

    added category class

---
+++ classes/Category.php
@@ -0,0 +1,15 @@
+<?php 
+
+class Category {
+    private $category;
+
+    public function __construct($category){
+        $this -> category = $category;
+    }
+
+    public function getCategory(){
+        return $this -> category;
+    }
+}
+
+?>
--- classes/Product.php
+++ classes/Product.php
@@ -5,7 +5,16 @@ class Product {
     private $price;
 
     public function __construct($name, $price){
-        
+        $this -> name = $name;
+        $this -> price = $price;
+    }
+
+    public function getName(){
+        return $this -> name;
+    }
+
+    public function getPrice(){
+        return $this -> price;
     }
 }
 
